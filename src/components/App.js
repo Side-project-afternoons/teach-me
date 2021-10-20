e4313d7a8c839015812c5b393921bb8f326c23dc
Alex <omtmn@icloud.com>
2021-10-20 17:18:34 -0400

    cleanup + routes

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import Navbar from './Navbar/Navbar'
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar/>
+      <Switch>
+        <Route exact path='/'>
+          {/* Home */}
+        </Route>
+        <Route exact path='/login'>
+          {/* Login Page */}
+        </Route>
+        <Route exact path='/register'>
+          {/* Register Page */}
+        </Route>
+        <Route exact path='/business'>
+          {/* TeachMe Business Page */}
+        </Route>
+        <Route exact path='/teach'>
+          {/* Teach on TeachMe Page */}
+        </Route>
+      </Switch>
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
