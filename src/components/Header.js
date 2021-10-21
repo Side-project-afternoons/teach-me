b76b604d803a790a7ed9944752a16bfb6c5099f6
Joe Panetta <jlpanetta1681@icoud.com>
2021-10-20 22:12:14 -0400

    initial setup for the components with the Header basic frame

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+        <h1>
+            Teach Me
+        </h1>
+        <div>
+            <input type="search" placeholder="Search" results="0" />
+        </div>
+
+    </div>
+    )
+}
+
+export default Header
