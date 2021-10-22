68f20dd837d925debf2834af615614cf3dd24a1f
ORiveraJr84 <oriverajr84@gmail.com>
2021-10-22 08:51:02 -0400

    Temporarily added Course selection component to main app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Header from "./components/Header"
-import './App.css';
-
-
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import "./App.css";
+import Selectionofcourses from "./components/SelectionOfCourses";
 
 function App() {
   return (
     <div className="App">
+      <Selectionofcourses />
       <Router>
         <Switch>
-          <Route  path="/">
+          <Route path="/">
             <Header />
           </Route>
         </Switch>
-        </Router>
-   </div>
+      </Router>
+    </div>
   );
 }
 export default App;
-            
-
