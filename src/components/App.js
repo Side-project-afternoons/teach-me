517fbb9809a667b4c817786ffb04be68b6a965af
Alex <omtmn@icloud.com>
2021-10-22 20:53:10 -0400

    navbar - react router. Somewhat responsive, a little janky

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Route, Switch } from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,60 @@
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import teachme  from '../../assets/teachme-logo.png'
+
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import LanguageIcon from '@mui/icons-material/Language';
 
 function Navbar() {
+    const [open, setOpen] = useState(false)
     return (
-        <header className="header">
+        <div className="header">
             <nav className="navbar">
-                <Link to="/" className="nav-logo">Teachme</Link>
-                <ul className="nav-menu">
-                    <li className="nav-item">
-                        <Link to="/business" className="nav-link">Teachme Business</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/teach" className="nav-link">Teach on teachme</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/login" className="nav-link">Login</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/register" className="nav-link">Sign up</Link>
-                    </li>
-                </ul>
-                <div class="hamburger">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
+                <Link to="/"><img className="nav-logo" src={teachme} alt='teachme logo'/></Link>
+                
+                <div className="container">
+                    <div className="searchInputWrapper">
+                        <input className="searchInput" type="text" placeholder='Search...'>
+                        </input>
+                    </div>
                 </div>
+
+                <div className="nav-menu-cont">
+                    <ul className="nav-menu">
+
+                        <li className="nav-item">
+                            <Link to="/business" className="nav-link">Teachme Business</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to="/teach" className="nav-link">Teach on teachme</Link>
+                        </li>
+
+                        <ShoppingCartOutlinedIcon className="nav-cart" />
+
+                        <li className="nav-item">
+                            <Link to="/login" className="nav-link-login">Login</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to="/register" className="nav-link-register">Sign up</Link>
+                        </li>
+
+                        <div className="nav-language-cont">
+                            <LanguageIcon className="nav-language"/>
+                        </div>
+
+                    </ul>
+                </div>
+                
+                <button onClick={() => setOpen(!open)}  className={`${open ? 'hamburger2' : 'hamburger'}`}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </button> 
             </nav>
-        </header>
+        </div>
     )
 }
-
 export default Navbar
