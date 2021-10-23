d95f5f4548cc399a30a1f705bad1c7ffd37829e4
Joe Panetta <jlpanetta1681@icloud.com>
2021-10-22 23:00:53 -0400

    finally got all the pieces on the board, now for more  styling and then functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,7 @@ display: flex;
 flex-direction: row;
 text-align: center;
 height: 100vh;
-margin-left:10px;
-align-items: space-evenly;
+justify-content: space-evenly;
 `;
 
 
@@ -18,7 +17,7 @@ const Searchbar = styled.div`
 display: column-flex;
 text-align: center;
 overflow: hidden;
-height: 100vh
+height: 100vh;
 padding-left: 10px;
 margin-top: 33px;
 padding-left: 10px;
@@ -26,26 +25,64 @@ height: 3.5vh;
 border: none
 `;
 
+const Button = styled.button`
+display: block;
+max-height: 10%;
+margin-left: 10px;
+
+     `
 
 const Logo = styled.div`
 display: flex;
 
 `
 
+const ContainerLeft = styled.div`
+overflow: hidden;  
+display: flex;
+flex-direction: row;
+text-align: center;
+height: 100vh;
+justify-content: space-around;
+`;
+
+const ContainerRight = styled.div`
+overflow: hidden;  
+display: flex;
+flex-direction: row;
+text-align: center;
+height: 100vh;
+margin-left:10px;
+justify-content: space-around;
+`;
+
+
+
 const Header = (props) => {
     return (
-  <Container>        
-    <Logo>
-        <h1 className="app-name">
-            Teach Me
-        </h1>
-    </Logo>
+<Container>
+        <ContainerLeft>
+            <Logo>
+                <h1 className="app-name">
+                    Teach Me
+                </h1>
+            </Logo>
 
-        <Searchbar>
-            <input type="text" placeholder="Search" results="0" />
-        </Searchbar>
+            <Searchbar>
+                <input type="search" placeholder="Search" results="0" />
+            </Searchbar>
+            <div>TeachMe Business</div>
+            <div>Solutions</div> 
+            <div>OnDemand Courses</div>
+            <div>Resource Plans</div>
+        </ContainerLeft>
 
-    </Container>
+            <ContainerRight>
+                <Button className="Log In">Log In</Button>
+                <Button className="Sign-up">Sign Up</Button>
+            </ContainerRight>
+</Container>
+
     )
 }
 
