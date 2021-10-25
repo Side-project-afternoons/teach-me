52cbc2fb2fb889bba6d90fe307bc789f3a8672e8
eet349 <51262590+eet349@users.noreply.github.com>
2021-10-25 13:16:57 -0500

    Adds responsiveness to the blue banner.

--- src/components/BlueBanner/BlueBanner.js
+++ src/components/BlueBanner/BlueBanner.js
@@ -8,12 +8,12 @@ import './BlueBanner.css';
 const BlueBanner = () => {
 	return (
 		<StyledBanner className='bg-blue color-white'>
-			<div className='flex-with-row'>
+			<div className='flex-with-row-to-col w-80 m-auto'>
 				<StyledH3 className='h3-cursive'>
 					We're your strategic learning partner. Let's move skills forward
 					together.
 				</StyledH3>
-				<StyledButton className='bg-blue color-white button-text'>
+				<StyledButton className='bg-blue color-white button-text min-w-160'>
 					Request a demo
 				</StyledButton>
 			</div>
