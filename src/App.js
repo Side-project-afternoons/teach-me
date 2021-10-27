af6345bf7993b53148bf2e2bfa4047827cadf7be
eet349 <51262590+eet349@users.noreply.github.com>
2021-10-27 18:37:08 -0500

    Adds a pages file to the file structure to clean up out files going forward.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero";
-import SelectionOfCourses from "./components/SelectionOfCourses";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+
 import "./App.css";
-import BlueBanner from "./components/BlueBanner/BlueBanner";
+import SignupPage from "./pages/SignupPage";
+import BusinessPage from "./pages/BusinessPage";
+import TeachmePage from "./pages/TeachmePage";
 
 function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Navbar />{" "}
-          {/* the other header is for the other two pages, waiting on merge */}
-          <Hero />
-          <SelectionOfCourses />
-          {/* <BlueBanner /> <------ I'm pretty sure this component belongs on other pages not on the main page */}
-        </Route>
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/">
+					<Navbar />{" "}
+					{/* the other header is for the other two pages, waiting on merge */}
+					<HomePage />
+				</Route>
 
-        <Route exact path="/login">
-          <Navbar />
-        </Route>
+				<Route exact path="/login">
+					<Navbar />
+					<LoginPage />
+				</Route>
 
-        <Route exact path="/register">
-          <Navbar />
-        </Route>
+				<Route exact path="/register">
+					<Navbar />
+					<SignupPage />
+				</Route>
 
-        <Route exact path="/business">
-          <Header />
-        </Route>
+				<Route exact path="/business">
+					<Header />
+					<BusinessPage />
+				</Route>
 
-        <Route exact path="/teach">
-          <Header />
-        </Route>
-      </Switch>
-      <footer>{/* Placeholder for Footer component */}</footer>
-    </div>
-  );
+				<Route exact path="/teach">
+					<Header />
+					<TeachmePage />
+				</Route>
+			</Switch>
+			<footer>{/* Placeholder for Footer component */}</footer>
+		</div>
+	);
 }
 export default App;
