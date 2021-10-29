84f0ce25283235b3d043e6dd01fbb9c6d15f2f91
ORiveraJr84 <oriverajr84@gmail.com>
2021-10-29 17:06:45 -0400

    fixed errors and refactored to use styled components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,62 +1,156 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import teachme from "../../images/teachme-logo.png";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import shoppingCart from "../../images/shoppingCart.svg";
+import globe from "../../images/globe.svg";
+import { flexbox } from "@mui/system";
 
-import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import LanguageIcon from "@mui/icons-material/Language";
+const NavBar = styled.header`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 22px;
+`;
+
+const Wrapper = styled.div`
+  width: 1023px;
+  display: flex;
+  gap: 20px;
+`;
+
+const Logo = styled.h3`
+  font-family: "Pacifico", cursive;
+  font-size: 25px;
+  color: #424242;
+`;
+
+const SearchBarWrapper = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+`;
+
+const searchStyle = {
+  position: "absolute",
+  fontSize: "12px",
+  left: "10px",
+};
+
+const SearchBox = styled.input`
+  width: 243px;
+  height: 32px;
+  border-radius: 30px;
+  font-size: 12px;
+  padding: 0px 20px 0px 28px;
+`;
+
+const NavLinks = styled.nav`
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  font-family: "Manrope", sans-serif;
+
+  ul {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    li {
+      font-size: 16px;
+    }
+  }
+`;
+
+const iconStyle = {
+  width: "22px",
+  height: "28px",
+};
+
+const buttonStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "76px",
+  height: "33px",
+  fontSize: "14px",
+  fontWeight: "600",
+  fontStyle: "normal",
+  textDecoration: "none",
+  textAlign: "center",
+  background: "#ffffff",
+  border: "2px solid #000000",
+  borderRadius: "0px",
+  cursor: "pointer",
+};
+
+const invertedButtonStyle = {
+  color: "white",
+  background: "black",
+};
 
 function Navbar() {
   const [open, setOpen] = useState(false);
   return (
-    <div className="header">
-      <nav className="navbar">
+    <NavBar>
+      <Wrapper>
         <Link to="/">
-          <img className="nav-logo" src={teachme} alt="teachme logo" />
+          <Logo className="logo">Teachme</Logo>
         </Link>
-
-        <div className="searchInputWrapper">
-          <input
-            className="searchInput"
-            type="text"
-            placeholder="Search..."
-          ></input>
-        </div>
-
-        <div className="nav-menu-cont">
-          <ul className="nav-menu">
-            <li className="nav-item">
-              <Link to="/business" className="nav-link">
-                Teachme Business
-              </Link>
+        <SearchBarWrapper>
+          <FontAwesomeIcon icon={faSearch} style={searchStyle} />
+          <SearchBox type="text"></SearchBox>
+        </SearchBarWrapper>
+        <NavLinks>
+          <ul>
+            <li>
+              <Link to="/business">Teachme Business</Link>
             </li>
-
-            <li className="nav-item">
-              <Link to="/teach" className="nav-link">
-                Teach on teachme
-              </Link>
+            <li>
+              <Link to="/teach">Teach on teachme</Link>
             </li>
-
-            <ShoppingCartOutlinedIcon className="nav-cart" />
-
-            <li className="nav-item">
+            <li>
+              <a href="/">
+                <img
+                  src={shoppingCart}
+                  className="cart"
+                  alt="shopping cart icon"
+                  style={iconStyle}
+                />
+              </a>
+            </li>
+            <li>
               <Link to="/login" className="nav-link-login">
-                Login
+                <button style={buttonStyle}>Login</button>
               </Link>
             </li>
-
-            <li className="nav-item">
+            <li>
               <Link to="/register" className="nav-link-register">
-                Sign up
+                <button style={{ ...buttonStyle, ...invertedButtonStyle }}>
+                  Sign up
+                </button>
               </Link>
             </li>
-
-            <div className="nav-language-cont">
-              <LanguageIcon className="nav-language" />
-            </div>
+            <li className="nav-language-cont">
+              <button style={{ ...buttonStyle, width: "33px" }}>
+                <img
+                  src={globe}
+                  className="globe"
+                  alt="globe icon"
+                  style={{
+                    ...iconStyle,
+                    width: "26px",
+                    height: "18px",
+                  }}
+                />
+              </button>
+            </li>
           </ul>
-        </div>
-
+        </NavLinks>
         <button
           onClick={() => setOpen(!open)}
           className={`${open ? "hamburger2" : "hamburger"}`}
@@ -65,8 +159,8 @@ function Navbar() {
           <span className="bar"></span>
           <span className="bar"></span>
         </button>
-      </nav>
-    </div>
+      </Wrapper>
+    </NavBar>
   );
 }
 export default Navbar;
